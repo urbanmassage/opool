6489bf162b31539263209d977e61615d86efb681
Louy <louy08@gmail.com>
2015-11-16 17:36:48 +0200

    Initial version

---
+++ index.ts
@@ -0,0 +1,25 @@
+/// <reference path="typings/tsd.d.ts" />
+
+class Pool<T extends {reset: Function}> {
+  private pool: T[];
+  private Func: new () => T;
+
+  constructor(Func: new () => T) {
+    this.pool = [];
+    this.Func = Func;
+  }
+
+  get(): T {
+    if (this.pool.length) {
+      return this.pool.splice(0, 1)[0];
+    }
+    return new this.Func();
+  }
+
+  release(obj: T): void {
+    if (obj.reset) obj.reset();
+    this.pool.push(obj);
+  }
+}
+
+export default Pool;
---
+++ test/index.ts
@@ -0,0 +1,41 @@
+import Pool from '../index';
+import {expect} from 'chai';
+
+describe('Pool', () => {
+  class Test {
+    test = null;
+    construct() {
+      this.reset();
+    }
+    reset() {
+      this.test = null;
+    }
+  }
+
+  it('accepts a class constructor', () => {
+    const pool = new Pool(Test);
+
+    const obj = pool.get();
+    expect(obj).to.be.an.instanceof(Test);
+  });
+
+  it('accepts returns unique objects', () => {
+    const pool = new Pool(Test);
+
+    const obj = pool.get();
+    expect(obj).to.be.an.instanceof(Test);
+    const obj2 = pool.get();
+    expect(obj2).to.be.an.instanceof(Test);
+    expect(obj2).to.not.equal(obj);
+  });
+
+  it('recycles', () => {
+    const pool = new Pool(Test);
+
+    const obj = pool.get();
+    pool.release(obj);
+
+    const obj2 = pool.get();
+    expect(obj2).to.equal(obj);
+  });
+});
